1058b0c3b8e57e09b59c15e86591414d93b175a0
paulahemsi <paulahemsi@gmail.com>
2022-01-03 20:00:18 -0300

    iterators(traits): adding const especialization

--- iterators_traits.hpp
+++ iterators_traits.hpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/03 19:53:07 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/01/03 19:54:08 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/01/03 19:59:45 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,31 @@
 
 template <class T>
 struct iterator_traits {
-  typedef typename T::value_type value_type;
-  typedef typename T::difference_type difference_type;
-  typedef typename T::iterator_category iterator_category;
-  typedef typename T::pointer pointer;
-  typedef typename T::reference reference;
+  typedef typename T::value_type		value_type;
+  typedef typename T::difference_type	difference_type;
+  typedef typename T::iterator_category	iterator_category;
+  typedef typename T::pointer			pointer;
+  typedef typename T::reference			reference;
 };
 
 template <typename T>
 struct iterator_traits<T*>
 {
-    typedef std::random_access_iterator_tag iterator_category;
-    typedef T                               value_type;
-    typedef T*                              pointer;
-    typedef T&                              reference;
-    typedef std::ptrdiff_t                  difference_type;
+    typedef std::random_access_iterator_tag	iterator_category;
+    typedef T								value_type;
+    typedef T*								pointer;
+    typedef T&								reference;
+    typedef std::ptrdiff_t					difference_type;
+};
+
+template <class T>
+class iterator_traits<const T*>
+{
+	typedef std::random_access_iterator_tag			iterator_category;
+	typedef T										value_type;
+	typedef const T*								pointer;
+	typedef const T&								reference;
+	typedef std::ptrdiff_t							difference_type;
 };
 
 #endif
