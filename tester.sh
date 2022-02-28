685ae48f4c85f6e542f66d1e2e3ec7ad94ba6b66
paulahemsi <paulahemsi@gmail.com>
2022-02-28 20:03:56 -0300

    Refactor(test script): loud compilation and log directory

---
+++ tester.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+RESET="\033[0m"
+V_GREEN="\e[0;38;5;82m"
+P_GREEN="\e[0;38;5;23m"
+
+LOGSDIR=./logs/
+
+echo -e "\n${V_GREEN} ----------making std version----------\n${RESET}"
+
+make original
+
+echo -e "\n${V_GREEN} ----------making ft version----------\n${RESET}"
+
+make
+
+echo -e "\n${V_GREEN} ----------executing and comparing----------\n${RESET}"
+./original > ${LOGSDIR}std.txt 2> ${LOGSDIR}exceptions.txt
+./ft.out > ${LOGSDIR}ft.txt 2>> ${LOGSDIR}exceptions.txt
+
+diff --color ${LOGSDIR}std.txt ${LOGSDIR}ft.txt
