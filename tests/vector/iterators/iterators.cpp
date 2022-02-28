7f7339c4779c17c11a619682450e5852a735bdc8
Lais Arena <laisarena@gmail.com>
2022-02-28 18:03:20 -0300

    refactor (iterators tests): add explicit test to iterate a vector

--- tests/vector/iterators/iterators.cpp
+++ tests/vector/iterators/iterators.cpp
@@ -6,16 +6,48 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:40:06 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/02/25 10:45:09 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/02/28 17:58:21 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iterators.hpp"
 
+static void iterate_through_vector_changing_values(void)
+{
+	print_subheading("Iterate through vector changing values");
+	
+	ft::vector<int> vector;
+	
+	for (int i=1; i<=5; i++)
+		vector.push_back(i);
+
+	for (ft::vector<int>::iterator it = vector.begin() ; it != vector.end(); ++it)
+		*it *= 2;
+	for (ft::vector<int>::const_iterator it = vector.begin() ; it != vector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << std::endl;	
+}
+
+static void iterate_through_vector(void)
+{
+	print_subheading("Iterate through vector");
+	
+	ft::vector<int> vector;
+	
+	for (int i=1; i<=5; i++)
+		vector.push_back(i);
+
+	for (ft::vector<int>::const_iterator it = vector.begin() ; it != vector.end(); ++it)
+		std::cout << ' ' << *it;
+	std::cout << std::endl;	
+}
+
 void test_iterators(void)
 {
-	std::cout.setf(std::ios::boolalpha);
-	
 	print_title("ITERATORS");
-	x<ft::vector<int>::iterator>();
+	iterate_through_vector();
+	iterate_through_vector_changing_values();
+	iterators_of_same_vector<ft::vector<int>::iterator>();
+	iterators_of_differents_vector<ft::vector<int>::iterator>();
+	iterator_when_change_value<ft::vector<int>::iterator>();
 }
