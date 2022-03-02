d1490f60f769ea0f94b28553c626c52d6ebb2808
Lais Arena <laisarena@gmail.com>
2022-03-02 17:03:00 -0300

    refactor (iterators tests): compare iterator memory address and print a bool

--- tests/vector/iterators/const_iterators.cpp
+++ tests/vector/iterators/const_iterators.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 10:37:10 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/02/28 21:32:42 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/03/02 16:45:38 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void test_const_iterators(void)
 {
 	print_title("CONST ITERATORS");
 	iterate_through_vector();
-	iterators_of_same_vector<ft::vector<int>::const_iterator>();
-	iterators_of_differents_vector<ft::vector<int>::const_iterator>();
-	iterator_when_change_value<ft::vector<int>::const_iterator>();
+	compare_iterators_of_same_vector<ft::vector<int>::const_iterator>();
+	compare_iterators_of_different_vector<ft::vector<int>::const_iterator>();
+	compare_iterator_when_change_element_value<ft::vector<int>::const_iterator>();
 }
