f8163add1ef2fc2d84b3a55172a3f1339d2ff8e2
Lais Arena <laisarena@gmail.com>
2022-03-02 21:20:12 -0300

    refactor(iterators tests): reorganize functions

--- tests/mk/vector_srcs.mk
+++ tests/mk/vector_srcs.mk
@@ -6,7 +6,7 @@
 #    By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/06 18:39:11 by lfrasson          #+#    #+#              #
-#    Updated: 2022/04/19 21:06:42 by lfrasson         ###   ########.fr        #
+#    Updated: 2022/04/19 21:11:16 by lfrasson         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -26,10 +26,8 @@ SRC			+=	ft_main.cpp\
 				simple_assignment_operator.cpp\
 				utils.cpp\
 				iterators.cpp\
-				const_iterators.cpp\
-				reverse_iterators.cpp\
-				const_reverse_iterators.cpp\
 				aux_iterators.cpp\
+				aux_reverse_iterators.cpp\
 				out_of_bounds.cpp\
 				relational_operators.cpp\
 				empty.cpp\
--- tests/vector/iterators/iterators.cpp
+++ tests/vector/iterators/iterators.cpp
@@ -6,12 +6,40 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/22 19:40:06 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/03/02 19:02:42 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/03/02 21:09:25 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "iterators.hpp"
-#include "iterators.tpp"
+
+void test_const_reverse_iterators(void)
+{
+	print_title("CONST REVERSE ITERATORS");
+	reverse_iterate_through_vector();
+	reverse_iterate_through_vector_changing_values();
+	// compare_iterators_of_same_vector<ft::vector<int>::const_reverse_iterator>();
+	// compare_iterators_of_different_vector<ft::vector<int>::const_reverse_iterator>();
+	// compare_iterator_when_change_element_value<ft::vector<int>::const_reverse_iterator>(7, 5);
+}
+
+void test_reverse_iterators(void)
+{
+	print_title("REVERSE ITERATORS");
+	reverse_iterate_through_vector();
+	reverse_iterate_through_vector_changing_values();
+	compare_iterators_of_same_vector<ft::vector<int>::reverse_iterator>();
+	compare_iterators_of_different_vector<ft::vector<int>::reverse_iterator>();
+	compare_iterator_when_change_element_value<ft::vector<int>::reverse_iterator>(7, 5);
+}
+
+void test_const_iterators(void)
+{
+	print_title("CONST ITERATORS");
+	iterate_through_vector();
+	compare_iterators_of_same_vector<ft::vector<int>::const_iterator>();
+	compare_iterators_of_different_vector<ft::vector<int>::const_iterator>();
+	compare_iterator_when_change_element_value<ft::vector<int>::const_iterator>(0, 2);
+}
 
 void test_iterators(void)
 {
