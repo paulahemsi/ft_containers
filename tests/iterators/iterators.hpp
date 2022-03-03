cf6c2c16b9bd5e70c4ed1479f1ed6d9cb45b0052
Lais Arena <laisarena@gmail.com>
2022-03-02 21:49:01 -0300

    refactor(iterators tests): make the same auxiliary templates works for all iterators types

--- tests/iterators/iterators.hpp
+++ tests/iterators/iterators.hpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/25 10:37:45 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/03/02 19:07:23 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/03/02 21:50:14 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,15 @@
 # define ITERATORS_HPP
 
 #include "tests.hpp"
-#include "iterators.tpp"
 
-void iterate_through_vector_changing_values(void);
-void iterate_through_vector(void);
-void reverse_iterate_through_vector_changing_values(void);
-void reverse_iterate_through_vector(void);
+void	getBegin(ft::vector<int>& vector, ft::vector<int>::iterator& iterator);
+void	getBegin(ft::vector<int>& vector, ft::vector<int>::const_iterator& iterator);
+void	getBegin(ft::vector<int>& vector, ft::vector<int>::reverse_iterator& iterator);
+void	getBegin(ft::vector<int>& vector, ft::vector<int>::const_reverse_iterator& iterator);
+void	getEnd(ft::vector<int>& vector, ft::vector<int>::iterator& iterator);
+void	getEnd(ft::vector<int>& vector, ft::vector<int>::const_iterator& iterator);
+void	getEnd(ft::vector<int>& vector, ft::vector<int>::reverse_iterator& iterator);
+void	getEnd(ft::vector<int>& vector, ft::vector<int>::const_reverse_iterator& iterator);
+void	change_value(ft::vector<int>& vector, int index1, int index2);
 
 #endif
