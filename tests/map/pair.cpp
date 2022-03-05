13f79f56cf7f2bf4a0d179db91655c25f44b58f1
paulahemsi <paulahemsi@gmail.com>
2022-03-05 18:30:16 -0300

    map(pair): add tests- still draft, needs refact

--- tests/map/pair.cpp
+++ tests/map/pair.cpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 21:00:06 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/03/05 17:44:51 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/03/05 18:29:44 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,17 +22,40 @@ int test_pair(void)
   ft::pair <std::string,double> product3 (product2);
 
   //product1 = std::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
+  std::cout << "P2 == P3: " << (product2 == product3) << std::endl;
+  std::cout << "P2 != P3: " << (product2 != product3) << std::endl;
+  std::cout << "P2 > P3: " << (product2 > product3) << std::endl;
+  std::cout << "P2 >= P3: " << (product2 >= product3) << std::endl;
+  std::cout << "P2 < P3: " << (product2 < product3) << std::endl;
+  std::cout << "P2 <= P3: " << (product2 <= product3) << std::endl;
+
+  product2.second = 39.90;
+  std::cout << "P2 == P3: " << (product2 == product3) << std::endl;
+  std::cout << "P2 != P3: " << (product2 != product3) << std::endl;
+  std::cout << "P2 > P3: " << (product2 > product3) << std::endl;
+  std::cout << "P2 >= P3: " << (product2 >= product3) << std::endl;
+  std::cout << "P2 < P3: " << (product2 < product3) << std::endl;
+  std::cout << "P2 <= P3: " << (product2 <= product3) << std::endl;
 
   product2.first = "shoes";
-  product2.second = 39.90;
+  std::cout << "P2 == P3: " << (product2 == product3) << std::endl;
+  std::cout << "P2 != P3: " << (product2 != product3) << std::endl;
+  std::cout << "P2 > P3: " << (product2 > product3) << std::endl;
+  std::cout << "P2 >= P3: " << (product2 >= product3) << std::endl;
+  std::cout << "P2 < P3: " << (product2 < product3) << std::endl;
+  std::cout << "P2 <= P3: " << (product2 <= product3) << std::endl;  
 
   ft::pair <std::string,double> product4;
   product4 = product2;
-
 
   std::cout << "P1: The price of " << product1.first << " is $" << product1.second << '\n';
   std::cout << "P2: The price of " << product2.first << " is $" << product2.second << '\n';
   std::cout << "P3: The price of " << product3.first << " is $" << product3.second << '\n';
   std::cout << "P4: The price of " << product4.first << " is $" << product4.second << '\n';
+
+  std::cout << "P1 == P2: " << (product1 == product2) << std::endl;
+  std::cout << "P2 == P3: " << (product2 == product3) << std::endl;
+  std::cout << "P3 == P4: " << (product3 == product4) << std::endl;
+  std::cout << "P2 == P4: " << (product2 == product4) << std::endl;
   return 0;
 }
