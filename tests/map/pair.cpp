3673135afec64734dcdbcf371d7662d7f08f87d6
paulahemsi <paulahemsi@gmail.com>
2022-03-05 17:45:34 -0300

    map(pair): add =operator overload

--- tests/map/pair.cpp
+++ tests/map/pair.cpp
@@ -6,11 +6,11 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 21:00:06 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/03/04 23:30:06 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/03/05 17:44:51 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <pair.tpp>
+#include "pair.tpp"
 
 #include <string>
 #include <iostream>
@@ -21,13 +21,18 @@ int test_pair(void)
   ft::pair <std::string,double> product2 ("tomatoes",2.30);
   ft::pair <std::string,double> product3 (product2);
 
-  //   product1 = std::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
+  //product1 = std::make_pair(std::string("lightbulbs"),0.99);   // using make_pair (move)
 
   product2.first = "shoes";
   product2.second = 39.90;
 
-  std::cout << "The price of " << product1.first << " is $" << product1.second << '\n';
-  std::cout << "The price of " << product2.first << " is $" << product2.second << '\n';
-  std::cout << "The price of " << product3.first << " is $" << product3.second << '\n';
+  ft::pair <std::string,double> product4;
+  product4 = product2;
+
+
+  std::cout << "P1: The price of " << product1.first << " is $" << product1.second << '\n';
+  std::cout << "P2: The price of " << product2.first << " is $" << product2.second << '\n';
+  std::cout << "P3: The price of " << product3.first << " is $" << product3.second << '\n';
+  std::cout << "P4: The price of " << product4.first << " is $" << product4.second << '\n';
   return 0;
 }
