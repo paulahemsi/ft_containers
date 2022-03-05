3dfd9313a3d5fad195b3d86c2c8f8a104ea4143c
paulahemsi <paulahemsi@gmail.com>
2022-03-05 20:44:34 -0300

    map(auxiliary): for now copying auxiliary file to be used by map

---
+++ tests/map/auxiliary.cpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   auxiliary.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 13:57:55 by lfrasson          #+#    #+#             */
+/*   Updated: 2022/03/05 20:22:43 by phemsi-a         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "tests.hpp"
+
+void print_instructions(std::string instruction)
+{
+	std::cout << L_GRAY << instruction << RESET << std::endl;
+}
+
+void print_title(std::string title)
+{
+	std::cout << CYAN << "\n\n*******" << title << "*******\n" RESET << std::endl;
+}
+
+void print_subheading(std::string subheading)
+{
+	std::cout << CYAN << "\n\n---" << subheading << "---\n" RESET << std::endl;
+}
+
