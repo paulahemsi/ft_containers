9e550f63aa92bd99affb6c822eca607520657fa7
Lais Arena <laisarena@gmail.com>
2022-03-06 15:38:41 -0300

    map(test script): change script to be able to test vector or map

--- tester.sh
+++ tester.sh
@@ -6,30 +6,50 @@ RED="\033[0;31m"
 V_GREEN="\e[0;38;5;82m"
 P_GREEN="\e[0;38;5;23m"
 
-DIFF=diff --collor
-if [ "$(eval uname)" == "Darwin" ]
-then
-    DIFF=diff
-    V_GREEN="\033[32m"
+usage()
+{
+	echo -e "usage:"
+	echo -e "bash tester.sh vector"
+	echo -e "bash tester.sh map"
+	exit
+}
+
+if (( $# != 1 )); then
+    usage
 fi
 
+ARG=$1
+if [ ${ARG} == "vector" ]; then
+    DIR=./tests/vector/
+elif [ ${ARG} == "map" ]; then
+    DIR=./tests/map/
+else
+	echo -e "\""$1"\" is not a valid argument."
+    usage
+fi
+
+if [ "$(eval uname)" == "Darwin" ]; then
+    DIFF=diff
+    V_GREEN="\033[32m"
+else
+    DIFF=diff --collor
+fi
 
 LOGSDIR=./logs/
-VECTORDIR=./tests/vector/
 
 echo -e "\n${V_GREEN} ----------making std version----------\n${RESET}"
 
-make -C ${VECTORDIR} original
+make -C ${DIR} original
 
 echo -e "\n${V_GREEN} ----------making ft version----------\n${RESET}"
 
-make -C ${VECTORDIR} 
+make -C ${DIR}
 
 echo -e "\n${V_GREEN} ----------executing and comparing----------\n${RESET}"
 
 mkdir -p ${LOGSDIR}
 
-${VECTORDIR}original > ${LOGSDIR}std.txt 2> ${LOGSDIR}exceptions.txt
-${VECTORDIR}ft.out > ${LOGSDIR}ft.txt 2>> ${LOGSDIR}exceptions.txt
+${DIR}original > ${LOGSDIR}std.txt 2> ${LOGSDIR}exceptions.txt
+${DIR}ft.out > ${LOGSDIR}ft.txt 2>> ${LOGSDIR}exceptions.txt
 
 ${DIFF} ${LOGSDIR}std.txt ${LOGSDIR}ft.txt
