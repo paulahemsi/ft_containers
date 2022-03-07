0cdad6e7a927f7530d0fd46ea18dee146b5050ee
Lais Arena <laisarena@gmail.com>
2022-03-06 23:33:29 -0300

    structure(Makefile): move all .mk files to specific directory

--- tests/mk/map_srcs.mk
+++ tests/mk/map_srcs.mk
@@ -1,47 +1,28 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    srcs.mk                                            :+:      :+:    :+:    #
+#    map_srcs.mk                                        :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/06 18:39:11 by lfrasson          #+#    #+#              #
-#    Updated: 2022/03/06 22:16:21 by lfrasson         ###   ########.fr        #
+#    Updated: 2022/03/06 23:38:21 by lfrasson         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
-include ../auxiliary.mk
+MK_DIR	:=	../mk/
+
+include $(MK_DIR)auxiliary.mk
 
 TEST_DIR	:=	./
 
-SUB_DIR		:=	member_functions/\
-				non-member_functions/
-SUB_DIR		:=	$(addprefix $(TEST_DIR), $(SUB_DIR))
+# SUB_DIR	:=	
+# SUB_DIR	:=	$(addprefix $(TEST_DIR), $(SUB_DIR))
 
-SRC			+=	ft_main.cpp\
-				constructors.cpp\
-				simple_assignment_operator.cpp\
-				utils.cpp\
-				iterators.cpp\
-				out_of_bounds.cpp\
-				relational_operators.cpp\
-				empty.cpp\
-				assign.cpp\
-				clear.cpp\
-				data.cpp\
-				erase.cpp\
-				insert.cpp\
-				swap.cpp\
-				resize.cpp\
-				reserve.cpp\
-				get_allocator.cpp\
-				element_access.cpp\
-				push_back.cpp\
-				pop_back.cpp
+SRC			+=	main.cpp\
+				pair.cpp
 
-HEADER		+=	tests.hpp\
-				comparison.hpp\
-				swap.tpp
+# HEADER	+=	
 
 INCLUDES	+= $(addprefix -I, $(TEST_DIR))
 
