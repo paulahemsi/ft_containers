7ad1582bfd700235c2aa97e4052ae91341242f1a
Lais Arena <laisarena@gmail.com>
2022-03-28 22:11:53 -0300

    map(structure)

--- containers/Map.hpp
+++ containers/Map.hpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:20:16 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/03/28 21:35:20 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/03/28 22:10:58 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include <iostream>
 #include <functional>
+#include "pair.tpp"
 #include "pair.tpp"
 
 namespace ft
@@ -25,6 +26,23 @@ namespace ft
              class Alloc = std::allocator<ft::pair<const Key,T> > >
     class map
     {
+        public:
+			typedef Key 											    key_type;
+			typedef T												    mapped_type;
+			typedef ft::pair<const key_type, mapped_type>	            value_type	
+            typedef std::size_t                                         size_type;
+            typedef std::ptrdiff_t                                      difference_type;
+            typedef Compare                                             key_compare;
+            typedef Allocator                                           allocator_type;
+            typedef typename value_type&                                reference;
+            typedef const typename value_type&                          const_reference;
+            typedef typename Allocator::pointer                         pointer;
+            typedef typename Allocator::const_pointer                   const_pointer;
+            typedef ft::map_iterator< ft::map<key_type, mapped_type> >  iterator;
+            typedef ft::map_iterator< ft::map<key_type, mapped_type> >  const_iterator;
+            typedef ft::reverse_iterator<iterator>                      reverse_iterator;
+            typedef ft::reverse_iterator<const_iterator>                const_reverse_iterator;
+
     };
 }
 
--- iterators/map_iterator.hpp
+++ iterators/map_iterator.hpp
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   bidirectional_iterator.hpp                         :+:      :+:    :+:   */
+/*   map_iterator.hpp                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/06 17:24:59 by phemsi-a          #+#    #+#             */
-/*   Updated: 2022/01/06 20:02:16 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/03/28 22:10:54 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BIDIRECTIONAL_ITERATOR
-#define BIDIRECTIONAL_ITERATOR
+#ifndef MAP_ITERATOR
+#define MAP_ITERATOR
 
 namespace ft {
 	
@@ -22,14 +22,14 @@ namespace ft {
 			pointer	_pointer;
 			
 		public:
-			typedef	random_access_iterator<T>	iterator;
+			typedef	map_iterator<T>	iterator;
 		
 			//canonical
-			random_access_iterator(): _pointer(NULL) {};
-			explicit random_access_iterator(pointer ptr): _pointer(ptr) {}
-			random_access_iterator(const random_access_iterator& instance) {*this = instance;}
-			~random_access_iterator() {};
-			random_access_iterator& operator=(const random_access_iterator& right_hand_side)
+			map_iterator(): _pointer(NULL) {};
+			explicit map_iterator(pointer ptr): _pointer(ptr) {}
+			map_iterator(const random_access_iterator& instance) {*this = instance;}
+			~map_iterator() {};
+			map_iterator& operator=(const random_access_iterator& right_hand_side)
 			{
 				this->_pointer = right_hand_side._pointer;
 				return *this;
