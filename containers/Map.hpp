44d5f77bfb12fe5c517da227e21bf37cbf8a87db
Lais Arena <laisarena@gmail.com>
2022-03-28 21:36:21 -0300

    map(structure): init map tamplate

--- containers/Map.hpp
+++ containers/Map.hpp
@@ -1,5 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Map.hpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 21:20:16 by lfrasson          #+#    #+#             */
+/*   Updated: 2022/03/28 21:35:20 by lfrasson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MAP_HPP
 #define MAP_HPP
+
+#include <iostream>
+#include <functional>
+#include "pair.tpp"
+
+namespace ft
+{
+    template<class Key,
+             class T,
+             class Compare = std::less<Key>,
+             class Alloc = std::allocator<ft::pair<const Key,T> > >
+    class map
+    {
+    };
+}
 
 #endif
