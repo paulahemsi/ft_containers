a613aea2c355c65058658c7714779c83fd92fe77
Lais Arena <laisarena@gmail.com>
2022-03-29 21:03:30 -0300

    map(constructors): add constructors prototypes

--- containers/Map.hpp
+++ containers/Map.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Map.hpp                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 21:20:16 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/03/28 22:17:40 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/03/29 21:01:55 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,19 +29,42 @@ namespace ft
         public:
 			typedef Key 											    key_type;
 			typedef T												    mapped_type;
-			typedef ft::pair<const key_type, mapped_type>	            value_type	
+			typedef ft::pair<const key_type, mapped_type>	            value_type;
             typedef std::size_t                                         size_type;
             typedef std::ptrdiff_t                                      difference_type;
             typedef Compare                                             key_compare;
-            typedef Allocator                                           allocator_type;
-            typedef typename value_type&                                reference;
-            typedef const typename value_type&                          const_reference;
-            typedef typename Allocator::pointer                         pointer;
-            typedef typename Allocator::const_pointer                   const_pointer;
+            typedef Alloc												allocator_type;
+			typedef typename allocator_type::reference					reference;
+			typedef typename allocator_type::const_reference			const_reference;
+            typedef typename Alloc::pointer								pointer;
+            typedef typename Alloc::const_pointer						const_pointer;
             typedef ft::map_iterator< ft::map<key_type, mapped_type> >  iterator;
             typedef ft::map_iterator< ft::map<key_type, mapped_type> >  const_iterator;
-            typedef ft::reverse_iterator<iterator>                      reverse_iterator;
-            typedef ft::reverse_iterator<const_iterator>                const_reverse_iterator;
+            // typedef ft::reverse_iterator<iterator>                      reverse_iterator;
+            // typedef ft::reverse_iterator<const_iterator>                const_reverse_iterator;
+        
+		private:
+			key_compare		_compare;
+			allocator_type	_allocator;
+		
+		public:
+            explicit map (const key_compare& comp = key_compare(),
+                          const allocator_type& alloc = allocator_type()):
+			_compare(comp),
+			_allocator(alloc) {}
+			
+			template <class InputIterator>
+  			map (InputIterator first,
+			  	 InputIterator last,
+       			 const key_compare& comp = key_compare(),
+       			 const allocator_type& alloc = allocator_type());
+			
+			map (const map& other)
+			{
+				*this = other;	
+			}
+
+
 
     };
 }
