c198b7a4950e7b58f5c8a48ada054cd4bf222580
paulahemsi <paulahemsi@gmail.com>
2022-03-30 22:58:54 -0300

    Makefile(mk): add btree files to mk

--- tests/mk/rb_tree.mk
+++ tests/mk/rb_tree.mk
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    rb_tree.mk                                         :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+         #
+#    By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/06 18:39:11 by lfrasson          #+#    #+#              #
-#    Updated: 2022/03/29 22:19:02 by lfrasson         ###   ########.fr        #
+#    Updated: 2022/03/30 22:49:04 by phemsi-a         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -14,7 +14,10 @@ DIR				:=	$(ROOT)rb_tree/includes/
 
 TEMPLATES		+=	rb_tree.hpp\
 					btree.tpp\
-					btree_insert_data.tpp
+					btree_insert_data.tpp\
+					btree_left_rotate.tpp\
+					btree_right_rotate.tpp\
+					btree_rotate_aux.tpp
 
 VPATH			+=	$(DIR)
 
