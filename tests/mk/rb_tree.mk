a72b59ff4fe619a4d7c674a57a97d8a89642185c
Lais Arena <laisarena@gmail.com>
2022-04-01 21:52:28 -0300

    rb_tree(Makefile): Include templates

--- tests/mk/rb_tree.mk
+++ tests/mk/rb_tree.mk
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    rb_tree.mk                                         :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+         #
+#    By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/03/06 18:39:11 by lfrasson          #+#    #+#              #
-#    Updated: 2022/03/30 22:49:04 by phemsi-a         ###   ########.fr        #
+#    Updated: 2022/04/01 20:50:42 by lfrasson         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,7 +17,10 @@ TEMPLATES		+=	rb_tree.hpp\
 					btree_insert_data.tpp\
 					btree_left_rotate.tpp\
 					btree_right_rotate.tpp\
-					btree_rotate_aux.tpp
+					btree_rotate_aux.tpp\
+					btree_next.tpp\
+					btree_previous.tpp\
+					btree_auxiliary.tpp
 
 VPATH			+=	$(DIR)
 
