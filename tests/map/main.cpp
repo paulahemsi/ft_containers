fb747d651fbee5730f78a6ce55f736ab3e30f962
Lais Arena <laisarena@gmail.com>
2022-04-03 12:45:50 -0300

    map(test): start iterator test

--- tests/map/main.cpp
+++ tests/map/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/04 22:16:02 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/04/01 23:37:45 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/04/03 12:11:21 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,22 @@ static void test_constructors(void)
 	
 }
 
+static void	test_iterators(void)
+{
+	print_title("ITERATORS");
+	
+	ft::map<int, int> map = create_map_of_random_integers(8, 200);
+	
+	for (ft::map<int, int>::iterator it = map.begin(); it != map.end(); ++it) {
+ 	   std::cout << it->first << ", " << it->second << '\n';
+	}
+}
+
 int main(void)
 {
 	print_title(VERSION);
 	std::cout.setf(std::ios::boolalpha);
 	test_pair();
 	test_constructors();
+	test_iterators();
 }
