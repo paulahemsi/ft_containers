2d4eebf64f72441609e248789eeaa6be15c93d4a
Lais Arena <laisarena@gmail.com>
2022-04-06 20:27:04 -0300

    test(auxiliary): put ORANGE define with other colors

--- tests/auxiliary/auxiliary.hpp
+++ tests/auxiliary/auxiliary.hpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/06 19:19:03 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/03/06 19:20:00 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/04/06 19:56:00 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 # define CYAN			"\e[0;36m"
 # define L_GRAY			"\e[0;38;5;245m"
 # define YELLOW			"\e[0;33m"
+# define ORANGE			"\e[0;38;5;166m"
 # define RESET			"\e[0m"
 
 void print_instructions(std::string instruction);
