03831782c5d9d050c5a6207d9c6560bb829440c0
paulahemsi <paulahemsi@gmail.com>
2022-04-15 10:45:33 -0300

    stack(canonical): add stack skeleton

--- containers/Stack.hpp
+++ containers/Stack.hpp
@@ -3,14 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   Stack.hpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 22:25:37 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/04/14 22:26:44 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/04/15 10:45:03 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STACK_HPP
 #define STACK_HPP
+
+#include "Vector.hpp"
+
+namespace ft
+{
+	template< class T, class Container = ft::vector<T> >
+	class stack
+	{
+		private:
+		
+		public:
+		
+		explicit stack( const Container& cont = Container() );
+
+		stack( const stack& other );
+		
+		~stack();
+		
+		stack& operator=( const stack& other );
+
+	};
+}
+
 
 #endif
