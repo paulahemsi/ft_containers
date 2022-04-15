55f3a67db3aa83a7ffd346efe23431529fa73887
paulahemsi <paulahemsi@gmail.com>
2022-04-15 11:22:12 -0300

    stack(canonical): add typename to typedefs

--- containers/Stack.hpp
+++ containers/Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 22:25:37 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/04/15 11:10:51 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/04/15 11:19:40 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,16 +20,18 @@ namespace ft
 	template< class T, class Container = ft::vector<T> >
 	class stack
 	{
-		private:
-			container_type						_container;
-
 		public:
 
-			typedef	Container					container_type
-			typedef	Container::value_type		value_type
-			typedef	Container::size_type		size_type
-			typedef	Container::reference		reference
-			typedef	Container::const_reference	const_reference
+			typedef	Container							container_type;
+			typedef	typename Container::value_type		value_type;
+			typedef	typename Container::size_type		size_type;
+			typedef	typename Container::reference		reference;
+			typedef	typename Container::const_reference	const_reference;
+
+		private:
+			container_type						_container;
+			
+		public:
 
 			explicit stack( const Container& cont = Container() ): _container(cont) {}
 
