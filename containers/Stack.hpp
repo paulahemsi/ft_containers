720f8a00795c533a7a1680a07c7d15dfe4eb140d
paulahemsi <paulahemsi@gmail.com>
2022-04-15 10:58:09 -0300

    fix(member types): change to public

--- containers/Stack.hpp
+++ containers/Stack.hpp
@@ -6,7 +6,7 @@
 /*   By: phemsi-a <phemsi-a@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 22:25:37 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/04/15 10:48:47 by phemsi-a         ###   ########.fr       */
+/*   Updated: 2022/04/15 10:57:48 by phemsi-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,21 +21,24 @@ namespace ft
 	class stack
 	{
 		private:
-
+			
+		public:
+		
 			typedef	Container					container_type
 			typedef	Container::value_type		value_type
 			typedef	Container::size_type		size_type
 			typedef	Container::reference		reference
 			typedef	Container::const_reference	const_reference
 
-		public:
-		
 		explicit stack( const Container& cont = Container() );
 
-		stack( const stack& other );
-		
+		stack( const stack& other )
+		{
+			
+		}
+
 		~stack();
-		
+
 		stack& operator=( const stack& other );
 	};
 }
