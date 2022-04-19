902bee0a2cad7a1b78c47344d308b5ea14ff43ad
Lais Arena <laisarena@gmail.com>
2022-04-18 22:39:25 -0300

    test(intra): add messages

--- tests/intra/main.cpp
+++ tests/intra/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lfrasson <lfrasson@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/18 21:02:07 by lfrasson          #+#    #+#             */
-/*   Updated: 2022/04/18 21:03:23 by lfrasson         ###   ########.fr       */
+/*   Updated: 2022/04/18 22:38:35 by lfrasson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,7 @@
 #endif
 
 #include <stdlib.h>
+#include "auxiliary.hpp"
 
 #define MAX_RAM 4294967296
 #define BUFFER_SIZE 4096
@@ -74,18 +75,22 @@ int main(int argc, char** argv) {
 	ft::stack<Buffer, std::deque<Buffer> > stack_deq_buffer;
 	ft::map<int, int> map_int;
 
+	print_title(" VECTOR OF BUFFER ");
+	print_instructions("Push values...");
 	for (int i = 0; i < COUNT; i++)
-	{
 		vector_buffer.push_back(Buffer());
-	}
 
+	print_instructions("Access random positions to replace values...");
 	for (int i = 0; i < COUNT; i++)
 	{
 		const int idx = rand() % COUNT;
 		vector_buffer[idx].idx = 5;
 	}
+
+	print_instructions("Swap content with empty vector");
 	ft::vector<Buffer>().swap(vector_buffer);
 
+	print_instructions("Try access some content");
 	try
 	{
 		for (int i = 0; i < COUNT; i++)
@@ -97,32 +102,41 @@ int main(int argc, char** argv) {
 	}
 	catch(const std::exception& e)
 	{
-		//NORMAL ! :P
+		std::cout << "OK!! Exception raised =) " <<std::endl;
 	}
 	
+	print_title(" MAP OF (int, int) ");
+	print_instructions("Insert pairs...");
 	for (int i = 0; i < COUNT; ++i)
-	{
 		map_int.insert(ft::make_pair(rand(), rand()));
-	}
 
+	print_instructions("Sum of values accessed randonly...");
 	int sum = 0;
 	for (int i = 0; i < 10000; i++)
 	{
-		int access = rand();
+		int access = rand() % COUNT;
 		sum += map_int[access];
 	}
-	std::cout << "should be constant with the same seed: " << sum << std::endl;
+	std::cout << "If run this test again, the follow value should be constant with the same seed: " << sum << std::endl;
+
+	print_instructions("Create a copy in a scope to check deep copy");
 
 	{
 		ft::map<int, int> copy = map_int;
 	}
+
+	print_instructions("Access some value of map");
+	sum += map_int[10];
+
+	print_title(" STACK ");
 	MutantStack<char> iterable_stack;
+	print_instructions("Push latter from \'a\' to \'z\'...");
 	for (char letter = 'a'; letter <= 'z'; letter++)
 		iterable_stack.push(letter);
+
+	print_instructions("Iterate with mutant stack to print values:");
 	for (MutantStack<char>::iterator it = iterable_stack.begin(); it != iterable_stack.end(); it++)
-	{
 		std::cout << *it;
-	}
 	std::cout << std::endl;
 	return (0);
 }
